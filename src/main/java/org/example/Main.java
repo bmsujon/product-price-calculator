1030b34cf822201df2f10c769e44bc28db8724cb
bmsujon <66627613+bmsujon@users.noreply.github.com>
2025-02-15 17:01:17 +0600

    testing workflow with some changes in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import org.example.enums.CurrencyEnum;
+import org.example.enums.PriceModel;
+import org.example.pojos.PriceConfig;
+import org.example.pojos.PriceTier;
+
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        PriceConfig priceConfig = new PriceConfig("p1");
+
+        priceConfig.addPriceTier(new PriceTier(11, 20, BigDecimal.valueOf(150.00), PriceModel.VOLUME));
+        priceConfig.addPriceTier(new PriceTier(1, 10, BigDecimal.valueOf(100.00), PriceModel.FLAT));
+        priceConfig.addPriceTier(new PriceTier(21, 30, BigDecimal.valueOf(250.00), PriceModel.VOLUME));
+
+        PriceCalculator priceCalculator = new PriceCalculator();
+
+        try {
+            System.out.println("Total Cost for 10 units: " + CurrencyEnum.USD + " " + priceCalculator.calculatePrice(priceConfig, 10));
+            System.out.println("Total Cost for 18 units: " + CurrencyEnum.USD + " " + priceCalculator.calculatePrice(priceConfig, 18));
+            System.out.println("Total Cost for 25 units: " + CurrencyEnum.USD + " " + priceCalculator.calculatePrice(priceConfig, 25));
+        } catch (Exception exception) {
+            System.out.println("Error occurred: " + exception.getMessage());
+        }
     }
 }
